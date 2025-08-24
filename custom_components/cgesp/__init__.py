3a3a3d9ec9a29ea12303db1b08fffefaa38d8cf8
stefhancampos <stefhanrafael@gmail.com>
2025-08-24 16:58:27 -0300

    Update __init__.py

--- custom_components/cgesp/__init__.py
+++ custom_components/cgesp/__init__.py
@@ -1,7 +1,6 @@
-from __future__ import annotations
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "cgesp"
+DOMAIN = "cgesp_weather"
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
